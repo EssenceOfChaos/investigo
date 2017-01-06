b0345ca76c8467b4e4c966ef090af405c6be0f4e
Frederick John <Freddy@Fredericks-iMac.local>
2017-01-05 21:00:07 -0500

    added friend features

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,82 +1,54 @@
 class UserStocksController < ApplicationController
   before_action :set_user_stock, only: [:show, :edit, :update, :destroy]
 
-  # GET /user_stocks
-  # GET /user_stocks.json
+
   def index
     @user_stocks = UserStock.all
   end
 
-  # GET /user_stocks/1
-  # GET /user_stocks/1.json
   def show
   end
 
-  # GET /user_stocks/new
+
   def new
     @user_stock = UserStock.new
   end
 
-  # GET /user_stocks/1/edit
+
   def edit
   end
 
-  # POST /user_stocks
-  # POST /user_stocks.json
-  def create
+  
+
 def create
+    if params[:stock_id].present?
+       @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
+    else
+      stock = Stock.find_by_ticker(params[:stock_ticker])
+      if stock
+        @user_stock = UserStock.new(user: current_user, stock: stock)
+      else
+        stock = Stock.new_from_lookup(params[:stock_ticker])
+        if stock.save
+          @user_stock = UserStock.new(user: current_user, stock: stock)
+        else
+          @user_stock = nil
+          flash[:error] = "Stock is not available"
+        end
+      end
+    end
 
-if params[:stock_id].present?
-
-@user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
-
-else
-
-stock = Stock.find_by_ticker(params[:stock_ticker])
-
-if stock
-
-@user_stock = UserStock.new(user: current_user, stock: stock)
-
-else
-
-stock = Stock.new_from_lookup(params[:stock_ticker])
-
-if stock.save
-
-@user_stock = UserStock.new(user: current_user, stock: stock)
-
-else
-
-@user_stock = nil
-
-flash[:error] = "Stock is not available"
-
-end
-
-end
-
-end
-
-respond_to do |format|
-
-if @user_stock.save
-
-format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.ticker} stock was successfully added" }
-
-format.json { render :show, status: :created, location: @user_stock }
-
-else
-
-format.html { render :new }
-
-format.json { render json: @user_stock.errors, status: :unprocessable_entity }
-
-end
-
-end
-
-end
+    respond_to do |format|
+      if @user_stock.save
+        format.html { redirect_to my_portfolio_path,
+          notice: "Stock #{@user_stock.stock.ticker} was succesfully added" }
+        format.json { render :show, status: :created, location: @user_stock }
+      else
+        format.html { render :new }
+        format.json { render json: @user_stock.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   # PATCH/PUT /user_stocks/1
   # PATCH/PUT /user_stocks/1.json
